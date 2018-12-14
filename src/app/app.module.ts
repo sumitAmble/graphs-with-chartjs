2d09ae5d49cacd31981f2a99d66857bb2dcc6e6b
Sumit Amble <amblesumit@gmail.com>
2018-12-14 12:47:13 +0530

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,20 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 
+import {ChartsModule} from 'ng2-charts';
+import { BarChartsComponent } from './charts/bar-charts/bar-charts.component';
+import { LineChartComponent } from './charts/line-chart/line-chart.component';
+import { PieChartComponent } from './charts/pie-chart/pie-chart.component';
+
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    BarChartsComponent,
+    LineChartComponent,
+    PieChartComponent
   ],
   imports: [
+    ChartsModule,
     BrowserModule
   ],
   providers: [],
---
+++ src/app/charts/pie-chart/pie-chart.component.ts
@@ -0,0 +1,24 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-pie-chart',
+  templateUrl: './pie-chart.component.html',
+  styleUrls: ['./pie-chart.component.css']
+})
+export class PieChartComponent {
+
+  // Pie
+  public pieChartLabels:string[] = ['Download Sales', 'In-Store Sales', 'Mail Sales'];
+  public pieChartData:number[] = [300, 500, 100];
+  public pieChartType:string = 'pie';
+ 
+  // events
+  public chartClicked(e:any):void {
+    console.log(e);
+  }
+ 
+  public chartHovered(e:any):void {
+    console.log(e);
+  }
+
+}
